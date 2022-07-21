5ad61276f259b0b764fe4e6096bddf5328621a28
myxtor <myxtor@MYXT>
2022-07-21 13:31:18 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.kartusara = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.erdemliDataSet1 = new WindowsFormsApp1.erdemliDataSet1();
+            this.erdemliDataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erdemliDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erdemliDataSet1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(309, 35);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(347, 22);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txt1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // kartusara
+            // 
+            this.kartusara.Location = new System.Drawing.Point(288, 248);
+            this.kartusara.Name = "kartusara";
+            this.kartusara.Size = new System.Drawing.Size(372, 22);
+            this.kartusara.TabIndex = 1;
+            this.kartusara.TextChanged += new System.EventHandler(this.kartusara_TextChanged);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(309, 66);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(347, 22);
+            this.txt2.TabIndex = 2;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            this.txt2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt2_KeyPress);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(685, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 50);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(685, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 50);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "kartuş adı";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.erdemli_belediyesi_logo_63D68B820A_seeklogo_com;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 132);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(195, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "kartuş adeti";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "kartuş sorgu";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(40, 320);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 93);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "göster";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(176, 300);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(688, 240);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // erdemliDataSet1
+            // 
+            this.erdemliDataSet1.DataSetName = "erdemliDataSet1";
+            this.erdemliDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // erdemliDataSet1BindingSource
+            // 
+            this.erdemliDataSet1BindingSource.DataSource = this.erdemliDataSet1;
+            this.erdemliDataSet1BindingSource.Position = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(662, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 53);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "kartusu ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 17);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "bir kez tıklayınız !";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(198, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "kartuş no";
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(309, 94);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(347, 22);
+            this.txt3.TabIndex = 19;
+            this.txt3.TextChanged += new System.EventHandler(this.txt3_TextChanged);
+            this.txt3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt3_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 50);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "ara";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(908, 320);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(149, 28);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "barkod üret";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(908, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(149, 228);
+            this.listBox1.TabIndex = 22;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1183, 562);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.kartusara);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erdemliDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erdemliDataSet1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox kartusara;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private erdemliDataSet1 erdemliDataSet1;
+        private System.Windows.Forms.BindingSource erdemliDataSet1BindingSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+//Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\bahadır\source\repos\WinFormsApp1\erdemli.accdb
+namespace WindowsFormsApp1
+{
+
+
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+        OleDbConnection baglanti = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\\Users\\bahadır\\source\\repos\\WinFormsApp1\\erdemli.accdb");
+        OleDbCommand komut;
+        OleDbDataAdapter adtr;
+        DataTable tablo = new DataTable();
+        OleDbCommandBuilder err;
+        DataSet ds = new DataSet();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            komut = new OleDbCommand("select *from kartus", baglanti);
+            adtr = new OleDbDataAdapter(komut);
+            adtr.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+            tablo.Rows.Add(txt1.Text, txt2.Text,txt3.Text);
+            dataGridView1.DataSource = tablo;
+            MessageBox.Show("işlem tamamdır");
+
+            //txt1.Text,
+
+
+
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kartusara_TextChanged(object sender, EventArgs e)
+        {
+            string aranan = kartusara.Text.Trim().ToUpper();
+            for (int i = 0; i <= dataGridView1.Rows.Count - 1; i++)
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    foreach (DataGridViewCell cell in dataGridView1.Rows[i].Cells)
+                    {
+                        if (cell.Value != null)
+                        {
+                            if (cell.Value.ToString().ToUpper() == aranan)
+                            {
+                                cell.Style.BackColor = Color.DarkTurquoise;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Lüffen silinecek satırı seçin.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //err = new OleDbCommandBuilder(adtr);
+            //adtr.Update(tablo);
+            //MessageBox.Show("işlem tamamdır");
+            //OleDbCommand komut = new OleDbCommand("insert into kartus (kimlik no,kartus adi,adeti,) values ('" + Convert.ToInt32(txt1.Text) + "','" + txt2.Text + "','" + txt3.Text + "',')", baglanti);
+            komut = new OleDbCommand();
+            baglanti.Open();
+            komut.Connection = baglanti;
+            komut.CommandText = "insert into kartus (kimlik,kartusadi, adeti) values (@pkimlik, @pkartusadi, @padeti)";
+
+            komut.Parameters.AddWithValue("@pkimlik", txt1.Text);
+            komut.Parameters.AddWithValue("@pkartusadi", txt2.Text);
+            komut.Parameters.AddWithValue("@padeti", txt3.Text);
+
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+
+
+            //OleDbCommand = new OleDbCommand("insert into kartus(kimlik,kartus adi,adeti) values("txt1.Text.ToString() + "," + txt2.Text.ToString() + ","txt3.Text.ToString() + ",",)", baglanti);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sayi, i;
+            listBox1.Items.Clear();
+            for (i = 0; i < 10; i++)
+            {
+                sayi = r.Next(-100, 100);
+                listBox1.Items.Add(sayi);
+            }
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            int sayi, i;
+            listBox1.Items.Clear();
+            for (i = 0; i < 10; i++)
+            {
+                sayi = r.Next(0, 10050);
+                listBox1.Items.Add(sayi);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+        }
+
+        private void txt2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+        }
+
+        private void txt3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+        }
+    }
+    
+}
